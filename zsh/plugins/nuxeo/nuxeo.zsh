079424b1b4889dd65c1ade84660b4285f4fe93ad
Thomas Roger <throger@gmail.com>
2012-01-28 22:25:37 +0100

    Add git functions to work with Nuxeo

--- zsh/plugins/nuxeo/nuxeo.zsh
+++ zsh/plugins/nuxeo/nuxeo.zsh
@@ -1,3 +1,26 @@
+NUXEO_HOME='/opt/nuxeo'
+
 alias cpjar='cp -r `find . -path ./*-distribution* -prune -o \( -iname "*SNAPSHOT.jar" -print \) `'
 alias ooheadless='/Applications/OpenOffice.org.app/Contents/MacOS/soffice.bin -headless -nofirststartwizard -accept="socket,host=localhost,port=8100;urp;StarOffice.Service"'
 
+
+gitf() {
+  for dir in . nuxeo-*; do
+    if [ -d "$dir"/.git ]; then
+      echo "[$dir]"
+      (cd "$dir" ; git "$@")
+      echo
+    fi
+  done
+}
+
+gitfa() {
+   ADDONS=$(mvn help:effective-pom -N|grep '<module>' |cut -d ">" -f 2 |cut -d "<" -f 1)
+   for dir in $ADDONS; do
+     if [ -d "$dir"/.git ]; then
+       echo "[$dir]"
+       (cd "$dir" ; git "$@")
+       echo
+     fi
+   done
+}
